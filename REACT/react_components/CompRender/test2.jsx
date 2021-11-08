918ca7e290558991ed6f4b0144689c10c574a4bf
mylitvinov <vinchester30@mail.ru>
2021-11-08 20:53:58 +0300

    CompRender

---
+++ REACT/react_components/CompRender/test2.jsx
@@ -0,0 +1,14 @@
+class OMG extends React.Component {
+  render() {
+    return <h1>Whooaa!</h1>;
+  }
+}
+ 
+class Crazy extends React.Component {
+  render() {
+    return <OMG />;
+  }
+}
+
+//==================================================================
+
