c82c5e1de39b9e0c4f43db0cadca4c235cf7df04
mylitvinov <vinchester30@mail.ru>
2021-11-10 20:02:32 +0300

    test1

---
+++ REACT/this.state/test1/Toogle.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+const green = '#39D1B4';
+const yellow = '#FFD712';
+
+class Toggle extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>
+          Change my color
+        </h1>
+      </div>
+    );
+  }
+}
