7925050c3cb64e8052dcb71ae585a92520c6406d
mylitvinov <vinchester30@mail.ru>
2021-11-13 13:29:24 +0300

    child react

--- REACT/ChildComponentsUpdate/Parent.jsx
+++ REACT/ChildComponentsUpdate/Parent.jsx
@@ -1,26 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Child } from './Child';
+import { Sibling } from './Sibling';
 
 class Parent extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { name: 'Frarthur' };
-    this.changeName = this.changeName.bind(this);
 
+    this.state = { name: 'Frarthur' };
+
+    this.changeName = this.changeName.bind(this);
   }
+
   changeName(newName) {
-  this.setState({
-    name: newName
-  });
-}
+    this.setState({
+      name: newName
+    });
+  }
 
   render() {
-    return <Child name={this.state.name} onChange = {this.changeName} />
+    return (
+      <div>
+        <Child 
+           onChange={this.changeName} />
+        <Sibling name={this.state.name} />
+      </div>
+    );
   }
 }
 
 ReactDOM.render(
-	<Parent />,
-	document.getElementById('app')
+  <Parent />,
+  document.getElementById('app')
 );
