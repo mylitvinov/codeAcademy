9618df9f7773d89ae87af086aee423a34f8a5015
mylitvinov <vinchester30@mail.ru>
2021-11-22 20:10:39 +0300

    project

--- REACT/jammming/src/Components/App/App.js
+++ REACT/jammming/src/Components/App/App.js
@@ -11,30 +11,30 @@ class App extends React.Component {
 
     this.state = {
       searchResults: [
-        { name: "name1", artist: "artist1", album: "album1", id: 1 },
-        { name: "name2", artist: "artist2", album: "album2", id: 2 },
-        { name: "name3", artist: "artist3", album: "album3", id: 3 },
+        // { name: "name1", artist: "artist1", album: "album1", id: 1 },
+        // { name: "name2", artist: "artist2", album: "album2", id: 2 },
+        // { name: "name3", artist: "artist3", album: "album3", id: 3 },
       ],
       playListName: "My playList",
       playListTracks: [
-        {
-          name: "playListName1",
-          artist: "playListArtist1",
-          album: "playListAlbum1",
-          id: 4,
-        },
-        {
-          name: "playListName2",
-          artist: "playListArtist2",
-          album: "playListAlbum2",
-          id: 5,
-        },
-        {
-          name: "playListName3",
-          artist: "playListArtist3",
-          album: "playListAlbum3",
-          id: 6,
-        },
+        // {
+        //   name: "playListName1",
+        //   artist: "playListArtist1",
+        //   album: "playListAlbum1",
+        //   id: 4,
+        // },
+        // {
+        //   name: "playListName2",
+        //   artist: "playListArtist2",
+        //   album: "playListAlbum2",
+        //   id: 5,
+        // },
+        // {
+        //   name: "playListName3",
+        //   artist: "playListArtist3",
+        //   album: "playListAlbum3",
+        //   id: 6,
+        // },
       ],
     };
 
@@ -67,6 +67,12 @@ class App extends React.Component {
   savePlayList() {
     // alert('CLICK')
     const trackUris = this.state.playListTracks.map((track) => track.uri);
+    Spotify.savePlayList(this.state.playListName, trackUris).then(() => {
+      this.setState({
+        playListName: "New Playlist",
+        playListTracks: [],
+      });
+    });
   }
 
   search(term) {
--- REACT/jammming/src/util/Spotify.js
+++ REACT/jammming/src/util/Spotify.js
@@ -67,6 +67,22 @@ const Spotify = {
     ).then(response => response.json()
     ).then(jsonResponse => {
       userId = jsonResponse.id;
+      return fetch(`https://api.spotify.com/v1/users/${userId}/playlists`,
+      {
+        headers: headers,
+        method: 'POST',
+        body: JSON.stringify({name: name}),
+
+      }).then(response => response.json()
+      ).then(jsonResponse =>{
+        const playlistId = jsonResponse.id;
+        return fetch(`/v1/users/${userId}/playlists/${playlistId}/tracks`,
+        {
+          headers: headers,
+          method: 'POST',
+          body: JSON.stringify({uris: trackUris}),
+        })
+      })
     })
   }
 };
